8c1abd41496e13725967c1376c10ddc897b31cb6
ella-may06 <reekieella@gmail.com>
2025-03-11 10:41:48 +0200

    created new class
     added public string strstringclassvar

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -9,8 +9,16 @@ package com.mycompany.prjclasses;
  * @author lab_services_student
  */
 public class PrjClasses {
+    
+    public String strSecondClassVar="Testing";
+    
 
     public static void main(String[] args) {
         System.out.println("Hello World! i just did github ");
+        newClass nc = new newClass();
+        
+        nc.helloClass();// have to use the variable name to communicate with the class.
+        
+        System.out.println(nc.strSecondClassVar);
     }
 }
